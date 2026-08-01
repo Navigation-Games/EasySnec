e7e5c429c7705ef932124096d3a93713b83c7246
Quinn Bowers <q.bowers@mailbox.org>
2026-08-01 15:59:09 -0400

    adding a few flags that should help with running offline

--- run_easysnec.ps1
+++ run_easysnec.ps1
@@ -1,8 +1,10 @@
 $env:PATH += "$env:USERPROFILE\.local\bin"
 
 if (Get-Command uv -errorAction SilentlyContinue) {
-    uv run easysnec
+    echo "uv already installed"    
 } else {
+    echo "uv not installed. fetching now..."
     powershell -ExecutionPolicy ByPass -c "irm https://astral.sh/uv/install.ps1 | iex"
-    uv run easysnec
 }
+
+uv run --offline --frozen --no-sync --no-dev --no-build easysnec
