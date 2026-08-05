ae6cf9fe12a42e29d0f350b32e12cf820262805d
Quinn Bowers <18728529+qbowers@users.noreply.github.com>
2026-08-05 19:34:22 -0400

    Fix runner (#57)
    * adding a few flags that should help with running offline
    
    * first time sync

--- run_easysnec.ps1
+++ run_easysnec.ps1
@@ -1,8 +1,18 @@
 $env:PATH += "$env:USERPROFILE\.local\bin"
 
 if (Get-Command uv -errorAction SilentlyContinue) {
-    uv run easysnec
+    echo "uv already installed"
 } else {
+    echo "uv not installed. fetching now..."
     powershell -ExecutionPolicy ByPass -c "irm https://astral.sh/uv/install.ps1 | iex"
-    uv run easysnec
 }
+
+uv sync --no-dev --check --offline
+if ($?) {
+    echo "project already initalized"
+} else {
+    echo "project uninitialized!"
+    uv sync --exact --no-dev
+}
+uv run --offline --frozen --no-sync --no-dev --no-build easysnec
+# uv run --offline --frozen --no-sync --no-dev --no-build easysnec
